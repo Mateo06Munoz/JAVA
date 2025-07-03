07b0266134b049e02e15808d17c816f53b144860
Mateo Muñoz <munozmateo000@gmail.com>
2025-07-02 20:08:38 -0500

    video sobre el poryecto maven

---
+++ wordEspaceJva/EjemploLogs/src/com/krakedev/ejercicios/TestAdmin.java
@@ -0,0 +1,9 @@
+package com.krakedev.ejercicios;
+
+public class TestAdmin {
+	public static void main(String[] args) {
+		Admin a=new Admin();
+		a.agregar();
+		
+	}
+}
---
+++ wordEspaceJva/ejemplomave/src/main/java/com/krakedev/ejemplomave/Admin.java
@@ -0,0 +1,18 @@
+package com.krakedev.ejemplomave;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Admin {
+	private static Logger logger=LogManager.getLogger(Admin.class);
+	
+	public void agregar() {
+		logger.warn("mendaje de warn");
+		logger.info("mendaje de info");
+		logger.debug("mendaje de debug");
+		logger.trace("mendaje de trace");	
+		
+		logger.error("mendaje de error");
+	}
+
+}
